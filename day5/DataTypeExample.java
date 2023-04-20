7301d02c51d9d14a8d1c880cfc23c1f729677ca9
TEJAPS <tejapolisetty143@gmail.com>
2023-04-20 09:06:40 +0530

    added day 5 content - data types

---
+++ day5/DataTypeExample.java
@@ -0,0 +1,50 @@
+public class DataTypeExample {
+    public static void main(String[] args) {
+        // byte
+        System.out.println("Byte signed minimum value: " + Byte.MIN_VALUE);
+        System.out.println("Byte signed maximum value: " + Byte.MAX_VALUE);
+        System.out.println("Byte unsigned minimum value: " + 0);
+        System.out.println("Byte unsigned maximum value: " + (Byte.MAX_VALUE * 2 + 1));
+
+        // short
+        System.out.println("Short signed minimum value: " + Short.MIN_VALUE);
+        System.out.println("Short signed maximum value: " + Short.MAX_VALUE);
+        System.out.println("Short unsigned minimum value: " + 0);
+        System.out.println("Short unsigned maximum value: " + (Short.MAX_VALUE * 2 + 1));
+
+        // int
+        System.out.println("Integer signed minimum value: " + Integer.MIN_VALUE);
+        System.out.println("Integer signed maximum value: " + Integer.MAX_VALUE);
+        System.out.println("Integer unsigned minimum value: " + 0);
+        System.out.println("Integer unsigned maximum value: " + (Integer.MAX_VALUE * 2L + 1));
+
+        // long
+        System.out.println("Long signed minimum value: " + Long.MIN_VALUE);
+        System.out.println("Long signed maximum value: " + Long.MAX_VALUE);
+        System.out.println("Long unsigned minimum value: " + 0L);
+        System.out.println("Long unsigned maximum value: " + (Long.MAX_VALUE * 2L + 1L));
+
+        // float
+        System.out.println("Float minimum value: " + Float.MIN_VALUE);
+        System.out.println("Float maximum value: " + Float.MAX_VALUE);
+
+        // double
+        System.out.println("Double minimum value: " + Double.MIN_VALUE);
+        System.out.println("Double maximum value: " + Double.MAX_VALUE);
+
+        // char
+        System.out.println("Character minimum value: " + (int) Character.MIN_VALUE);
+        System.out.println("Character maximum value: " + (int) Character.MAX_VALUE);
+
+        // boolean
+        System.out.println("Boolean minimum value: " + Boolean.FALSE);
+        System.out.println("Boolean maximum value: " + Boolean.TRUE);
+    }
+}
+
+class Karthik {
+	
+}
+
+class Bharat {
+}
