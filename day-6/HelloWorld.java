4e78cb29082e5eb34daf8054dd50cf4c68120de8
TEJAPS <tejapolisetty143@gmail.com>
2023-04-21 11:17:00 +0530

    added day6 content - constructor, theoritical inheritance

---
+++ day-6/HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld {
+	public static void main(String args[]){
+		System.out.println("hello world is working");
+		
+		//Person person = new Person();
+		
+		//Person person = new Person("bharat");
+		
+		//Person person = new Person(10);
+		
+		Person person = new Person("bharat",10);
+		
+		//person.setName("bharat");
+		//person.setAge(10);
+		//person.initialize("bharat",10);
+		
+		System.out.println("name :" + person.getName());
+		System.out.println("age :" + person.getAge());
+		
+		
+		Person person2 = new Person();
+		Person person = new Person("karthik",9);
+		System.out.println("name :" + person2.getName());
+		System.out.println("age :" + person2.getAge());
+		
+	}
+}
---
+++ day-6/Person.java
@@ -0,0 +1,48 @@
+public class Person {
+	private String name;
+	private int age;
+	
+	//default constructor
+	Person(){
+		System.out.println("in the default constructor of person");
+	}
+	
+	// parametarized constructor
+	Person(String abcd){
+		System.out.println("in the parametarized constructor of person having string");
+		name = abcd;
+	}
+	
+	Person(int inputAge){
+		System.out.println("in the parametarized constructor of person having int");
+		age = inputAge;
+	}
+	
+	Person(String abcd, int inputAge){
+		name=abcd;
+		age = inputAge;
+	}
+	
+	
+	String getName(){
+		return name;
+	}
+	
+	int getAge(){
+		return age;
+	}
+	
+	
+	void initialize(String abcd, int inputAge){
+		name= abcd;
+		age = inputAge;
+	}
+	
+	void setName(String inputName){
+		name = inputName;
+	}
+	
+	void setAge(int inputAge){
+		age = inputAge;
+	}
+}
